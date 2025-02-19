a976c48a7b9c857b960510d42267a4994c1baa8e
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-19 22:15:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+let boxes = document.querySelectorAll(".code");
+
+boxes.forEach((box,index) =>{
+	box.addEventListener("input" , () =>{
+		if(box.value){
+			if(index<boxes.length){
+				boxes[index+1].focus();
+			}
+		}
+	})
+
+	box.addEventListener("keydown",(e) =>{
+		let pressedKey = e.key;
+		if(pressedKey==='Backspace'){
+			e.preventDefault();
+			box.value=" ";
+			if(index>0){
+			boxes[index-1].focus();
+			}
+		}
+	})
+})
+
